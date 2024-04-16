2083c51bfbc06e30e63792af6edd36b6af01303c
bahetyshyam <bahetyshyam@gmail.com>
2024-04-15 22:40:47 -0400

    Updated form information according to new form format.

--- src/components/UserForm/index.tsx
+++ src/components/UserForm/index.tsx
@@ -1,37 +1,75 @@
 import { useId, useState } from 'react';
-import { formData } from '../../formData';
+import {
+  victimProtocol,
+  victimPreCondition,
+  victimPostCondition,
+  attackerKnowledge,
+  attackerHardware,
+  attackerPostCondition,
+} from '../../formData';
+
+function FormItemContainer({ formItem, onChangeOfCheckboxItem }) {
+  if (formItem.type === 'dropdown') {
+    return (
+      <div className="dropdown-item">
+        <input
+          id={formItem.value}
+          type="checkbox"
+          value={formItem.value}
+          checked={formItem.checked}
+          className=" bg-gray-100 border-gray-300 rounded"
+          name="first-level-dropdown"
+          onChange={(e) => onChangeOfCheckboxItem(e, formItem)}
+        />
+        <label htmlFor={formItem.value} className="ms-2">
+          {formItem.value}
+        </label>
+        {formItem.checked &&
+          formItem.children?.map((child) => {
+            return (
+              <div className="ml-4">
+                <FormChild child={child} />
+              </div>
+            );
+          })}
+      </div>
+    );
+  }
+  if (formItem.type === 'radio' || formItem.type === 'select') {
+    return <FormChild child={formItem} />;
+  }
+}
 
 function FormChild({ child }) {
-  const uniqueIdForChild = useId();
+  const uniqueIdForItem = useId();
   return (
-    <div className={`child-item ${child.title ? 'ml-5' : ''}`}>
+    <div className={`child-item`}>
       <div>{child.title}</div>
-      {(child.type === 'select' || child.type === 'radio') &&
-        child.options?.map((option) => {
-          return (
-            <FormOption
-              option={option}
-              type={child.type}
-              childId={uniqueIdForChild}
-            />
-          );
-        })}
+      {child.options?.map((option) => {
+        return (
+          <FormOption
+            option={option}
+            type={child.type}
+            parentId={uniqueIdForItem}
+          />
+        );
+      })}
     </div>
   );
 }
 
-function FormOption({ option, type, childId }) {
+function FormOption({ option, type, parentId }) {
   const id = useId();
   return (
-    <div className="ml-5 option-item">
+    <div className="option-item">
       {type === 'select' && (
         <div>
           <input
             id={id}
             type="checkbox"
             value={option.value}
-            name={childId}
-            className=" bg-gray-100 border-gray-300 rounded"
+            name={parentId}
+            className="bg-gray-100 border-gray-300 rounded"
           />
           <label htmlFor={id} className="ms-2">
             {option.value}
@@ -44,8 +82,8 @@ function FormOption({ option, type, childId }) {
             id={id}
             type="radio"
             value={option.value}
-            name={childId}
-            className=" bg-gray-100 border-gray-300 rounded"
+            name={parentId}
+            className="bg-gray-100 border-gray-300 rounded"
           />
           <label htmlFor={id} className="ms-2">
             {option.value}
@@ -60,41 +98,106 @@ function FormOption({ option, type, childId }) {
 }
 
 function UserForm() {
-  const [formDataState, setFormDataState] = useState<typeof formData>(formData);
-  function onChangeOfDropdownCheckbox(e, formItem) {
+  const [formDataState, setFormDataState] =
+    useState<typeof victimProtocol>(victimProtocol);
+  function onChangeOfCheckboxItem(e, formItem) {
     formItem.checked = e.target.checked;
     setFormDataState([...formDataState]);
   }
   return (
     <div>
-      <div className="form-container text-xl mt-3">
-        {formDataState.map((formItem) => {
-          return (
-            <div className="dropdown-item">
-              <input
-                id={formItem.value}
-                type="checkbox"
-                value={formItem.value}
-                checked={formItem.checked}
-                className=" bg-gray-100 border-gray-300 rounded"
-                name="first-level-dropdown"
-                onChange={(e) => onChangeOfDropdownCheckbox(e, formItem)}
-              />
-              <label htmlFor={formItem.value} className="ms-2">
-                {formItem.value}
-              </label>
-              {formItem.checked &&
-                formItem.children?.map((child) => {
-                  return (
-                    <div className="ml-4">
-                      <FormChild child={child} />
-                    </div>
-                  );
-                })}
-            </div>
-          );
-        })}
-      </div>
+      <section id="victim-information" className="form-container text-xl mt-3">
+        <h2 className="text-3xl font-semibold text-blue-500">
+          Victim Information
+        </h2>
+        <div id="victim-protocol">
+          <h3 className="text-2xl font-semibold mt-3">Protocol</h3>
+          <div className="form-container text-xl">
+            {victimProtocol.map((formItem) => {
+              return (
+                <FormItemContainer
+                  formItem={formItem}
+                  onChangeOfCheckboxItem={onChangeOfCheckboxItem}
+                />
+              );
+            })}
+          </div>
+        </div>
+        <div id="victim-precondition">
+          <h3 className="text-2xl font-semibold mt-3">Preconditions</h3>
+          <div className="form-container text-xl">
+            {victimPreCondition.map((formItem) => {
+              return (
+                <FormItemContainer
+                  formItem={formItem}
+                  onChangeOfCheckboxItem={onChangeOfCheckboxItem}
+                />
+              );
+            })}
+          </div>
+        </div>
+        <div id="victim-post-condition">
+          <h3 className="text-2xl font-semibold mt-3">Post Condition</h3>
+          <div className="form-container text-xl">
+            {victimPostCondition.map((formItem) => {
+              return (
+                <FormItemContainer
+                  formItem={formItem}
+                  onChangeOfCheckboxItem={onChangeOfCheckboxItem}
+                />
+              );
+            })}
+          </div>
+        </div>
+      </section>
+      <section
+        id="attacker-information"
+        className="form-container text-xl mt-3"
+      >
+        <h2 className="text-3xl font-semibold text-blue-500">
+          Attacker Information
+        </h2>
+        <div id="attacker-knowledge">
+          <h3 className="text-2xl font-semibold mt-3">Attacker Knowledge</h3>
+          <div className="form-container text-xl">
+            {attackerKnowledge.map((formItem) => {
+              return (
+                <FormItemContainer
+                  formItem={formItem}
+                  onChangeOfCheckboxItem={onChangeOfCheckboxItem}
+                />
+              );
+            })}
+          </div>
+        </div>
+        <div id="attacker-hardware">
+          <h3 className="text-2xl font-semibold mt-3">Attacker Hardware</h3>
+          <div className="form-container text-xl">
+            {attackerHardware.map((formItem) => {
+              return (
+                <FormItemContainer
+                  formItem={formItem}
+                  onChangeOfCheckboxItem={onChangeOfCheckboxItem}
+                />
+              );
+            })}
+          </div>
+        </div>
+        <div id="attacker-post-condition">
+          <h3 className="text-2xl font-semibold mt-3">Post Condition</h3>
+          <div className="form-container text-xl">
+            {attackerPostCondition.map((formItem) => {
+              return (
+                <FormItemContainer
+                  formItem={formItem}
+                  onChangeOfCheckboxItem={onChangeOfCheckboxItem}
+                />
+              );
+            })}
+          </div>
+        </div>
+      </section>
+
       <button
         type="button"
         className="text-white bg-blue-500 hover:bg-blue-600 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 w-full mt-6"
--- src/formData.ts
+++ src/formData.ts
@@ -1,252 +1,26 @@
 type OptionType = 'dropdown' | 'radio' | 'select' | 'input';
-export const formData = [
+export const victimProtocol = [
   // children show up only when it's current parent is selected
   // options show up in the case of select and radio
   {
     type: 'dropdown',
     checked: true,
     value: '2G GSM',
-    children: [
-      {
-        type: 'select',
-        title: 'Adversarial Knowledge',
-        options: [
-          {
-            value: 'IMSI',
-          },
-          {
-            value: 'TMSI',
-          },
-          {
-            value: 'IMEI',
-          },
-        ],
-      },
-      {
-        type: 'radio',
-        title: 'System State',
-        options: [
-          {
-            value: 'Unauthenticated',
-          },
-          {
-            value: 'Authenticated',
-            children: [
-              {
-                title: 'Confidentiality',
-                type: 'radio',
-                options: [
-                  {
-                    value: 'A5/0 Null',
-                  },
-                  {
-                    value: 'A5/1',
-                  },
-                  {
-                    value: 'A5/2',
-                  },
-                ],
-              },
-            ],
-          },
-        ],
-      },
-    ],
   },
   {
     type: 'dropdown',
     checked: false,
     value: '3G UMTS',
-    children: [
-      {
-        type: 'select',
-        title: 'Adversarial Knowledge',
-        options: [
-          {
-            value: 'IMSI',
-          },
-          {
-            value: 'TMSI',
-          },
-          {
-            value: 'IMEI',
-          },
-        ],
-      },
-      {
-        type: 'radio',
-        title: 'System State',
-        options: [
-          {
-            value: 'AKA Unauthenticated',
-          },
-          {
-            value: 'AKA Authenticated',
-            children: [
-              {
-                title: 'Confidentiality',
-                type: 'radio',
-                options: [
-                  {
-                    value: 'UEA0 Null',
-                  },
-                  {
-                    value: 'UEA1 Kasumi',
-                  },
-                ],
-              },
-            ],
-          },
-        ],
-      },
-    ],
   },
   {
     type: 'dropdown',
     checked: false,
     value: '4G LTE',
-    children: [
-      {
-        type: 'select',
-        title: 'Adversarial Knowledge',
-        options: [
-          {
-            value: 'IMSI',
-          },
-          {
-            value: 'GUTI',
-          },
-          {
-            value: 'IMEI',
-          },
-        ],
-      },
-      {
-        type: 'radio',
-        title: 'System State',
-        options: [
-          {
-            value: 'AKA Unauthenticated',
-          },
-          {
-            value: 'AKA Authenticated',
-            children: [
-              {
-                title: 'Confidentiality',
-                type: 'radio',
-                options: [
-                  {
-                    value: 'EEA0 Null',
-                  },
-                  {
-                    value: 'EEA1 SNOW3G',
-                  },
-                  {
-                    value: 'EEA2 AES',
-                  },
-                  {
-                    value: 'EEA3 ZUC',
-                  },
-                ],
-              },
-              {
-                title: 'Integrity',
-                type: 'radio',
-                options: [
-                  {
-                    value: 'EEA0 Null',
-                  },
-                  {
-                    value: 'EEA1 SNOW3G',
-                  },
-                  {
-                    value: 'EEA2 AES',
-                  },
-                  {
-                    value: 'EEA3 ZUC',
-                  },
-                ],
-              },
-            ],
-          },
-        ],
-      },
-    ],
   },
   {
     type: 'dropdown',
     value: '5G NR',
     checked: false,
-    children: [
-      {
-        type: 'select',
-        title: 'Adversarial Knowledge',
-        options: [
-          {
-            value: 'SUPI',
-          },
-          {
-            value: 'SUCI',
-          },
-          {
-            value: '5G-GUTI',
-          },
-          {
-            value: 'IMEI',
-          },
-        ],
-      },
-      {
-        type: 'radio',
-        title: 'System State',
-        options: [
-          {
-            value: 'AKA Unauthenticated',
-          },
-          {
-            value: 'AKA Authenticated',
-            children: [
-              {
-                title: 'Confidentiality',
-                type: 'radio',
-                options: [
-                  {
-                    value: 'NEA0 Null',
-                  },
-                  {
-                    value: 'NEA1 SNOW3G',
-                  },
-                  {
-                    value: 'NEA2 AES',
-                  },
-                  {
-                    value: 'NEA3 ZUC',
-                  },
-                ],
-              },
-              {
-                title: 'Integrity',
-                type: 'radio',
-                options: [
-                  {
-                    value: 'NEA0 Null',
-                  },
-                  {
-                    value: 'NEA1 SNOW3G',
-                  },
-                  {
-                    value: 'NEA2 AES',
-                  },
-                  {
-                    value: 'NEA3 ZUC',
-                  },
-                ],
-              },
-            ],
-          },
-        ],
-      },
-    ],
   },
   {
     type: 'dropdown',
@@ -255,67 +29,362 @@ export const formData = [
     children: [
       {
         type: 'select',
-        title: 'Adversarial Knowledge',
         options: [
           {
-            value: 'Mac Address',
+            value: 'Bluetooth Core Specification 5.4',
+            checked: false,
           },
           {
-            value: 'PIN',
+            value: 'Bluetooth Core Specification 5.3',
+            checked: false,
           },
           {
-            value: 'Encryption Key (Kc)',
-          },
-        ],
-      },
-      {
-        type: 'select',
-        title: 'System State',
-        options: [
-          {
-            value: 'Connectable',
+            value: 'Bluetooth Core Specification 5.2',
+            checked: false,
           },
           {
-            value: 'Discoverable',
+            value: 'Bluetooth Core Specification 5.1',
+            checked: false,
           },
           {
-            value: 'Paired',
+            value: 'Bluetooth Core Specification 5.0',
+            checked: false,
           },
           {
-            value: 'Transmitting',
+            value: 'Bluetooth Core Specification 4.2',
+            checked: false,
           },
           {
-            value: 'CTKD Support',
+            value: 'Bluetooth Core Specification 4.0',
+            checked: false,
           },
           {
-            value: 'Just Works Support',
+            value: 'Bluetooth Core Specification 3.0+HS',
+            checked: false,
+          },
+          {
+            value: 'Bluetooth Core Specification 2.1+EDR',
+            checked: false,
+          },
+          {
+            value: 'Bluetooth Core Specification 2.0+EDR',
+            checked: false,
+          },
+          {
+            value: 'Bluetooth Core Specification 1.2',
+            checked: false,
+          },
+          {
+            value: 'Bluetooth Core Specification 1.1',
+            checked: false,
           },
         ],
       },
     ],
   },
+  {
+    type: 'dropdown',
+    value: 'Bluetooth Low Energy (BLE)',
+    checked: false,
+    children: [
+      {
+        type: 'select',
+        options: [
+          {
+            value: 'Bluetooth Core Specification 5.4',
+            checked: false,
+          },
+          {
+            value: 'Bluetooth Core Specification 5.3',
+            checked: false,
+          },
+          {
+            value: 'Bluetooth Core Specification 5.2',
+            checked: false,
+          },
+          {
+            value: 'Bluetooth Core Specification 5.1',
+            checked: false,
+          },
+          {
+            value: 'Bluetooth Core Specification 5.0',
+            checked: false,
+          },
+          {
+            value: 'Bluetooth Core Specification 4.2',
+            checked: false,
+          },
+          {
+            value: 'Bluetooth Core Specification 4.0',
+            checked: false,
+          },
+        ],
+      },
+    ],
+  },
+  {
+    type: 'dropdown',
+    value: 'Wi-Fi',
+    checked: false,
+    children: [
+      {
+        type: 'select',
+        options: [
+          {
+            value: 'Wi-Fi 6 (IEEE 802.11ax)',
+            checked: false,
+          },
+          {
+            value: 'Wi-Fi 5 (IEEE 802.11ac)',
+            checked: false,
+          },
+          {
+            value: 'Wi-Fi 4 (IEEE 802.11n)',
+            checked: false,
+          },
+          {
+            value: 'Wi-Fi 3 (IEEE 802.11g)',
+            checked: false,
+          },
+          {
+            value: 'Wi-Fi 2 (IEEE 802.11a)',
+            checked: false,
+          },
+          {
+            value: 'Wi-Fi 1 (IEEE 802.11b)',
+            checked: false,
+          },
+        ],
+      },
+    ],
+  },
+  {
+    type: 'dropdown',
+    value: 'Zigbee',
+    checked: false,
+  },
 ];
 
-// {
-//   value: 'General Knowledge',
-//   type: 'dropdown',
-//   children: [
-//     {
-//       type: 'select',
-//       options: [
-//         {
-//           value: 'Phone Number',
-//           type: 'input',
-//         },
-//         {
-//           value: 'User Location (m range)',
-//           type: 'input',
-//         },
-//         {
-//           value: 'User Location (km range)',
-//           type: 'input',
-//         },
-//       ],
-//     },
-//   ],
-// },
+export const victimPreCondition = [
+  {
+    type: 'radio',
+    title: 'Authentication State',
+    value: 'Authenticated',
+    options: [
+      {
+        value: 'Authenticated',
+      },
+      {
+        value: 'Unauthenticated',
+      },
+    ],
+  },
+  {
+    type: 'select',
+    title: 'System State',
+    options: [
+      {
+        value: 'Off',
+      },
+      {
+        value: 'Idle',
+      },
+      {
+        value: 'Searching',
+      },
+      {
+        value: 'Connecting',
+      },
+      {
+        value: 'Connected',
+      },
+      {
+        value: 'Disconnected',
+      },
+    ],
+  },
+];
+
+export const victimPostCondition = [...victimPreCondition];
+
+export const attackerKnowledge = [
+  {
+    type: 'select',
+    options: [
+      {
+        value: 'IMSI',
+        checked: false,
+      },
+      {
+        value: 'TMSI (2G)',
+        checked: false,
+      },
+      {
+        value: 'TMSI (3G)',
+        checked: false,
+      },
+      {
+        value: 'TMSI (4G/5G-NSA)',
+        checked: false,
+      },
+      {
+        value: 'TMSI (5G)',
+        checked: false,
+      },
+      {
+        value: 'SUPI (5G)',
+        checked: false,
+      },
+      {
+        value: 'SUCI (5G-SA)',
+        checked: false,
+      },
+      {
+        value: 'MAC Address (Bluetooth Classic)',
+        checked: false,
+      },
+      {
+        value: 'MAC Address (BLE)',
+        checked: false,
+      },
+      {
+        value: 'MAC Address (Wi-Fi)',
+        checked: false,
+      },
+      {
+        value: 'IP Address (Wi-Fi)',
+        checked: false,
+      },
+      {
+        value: 'Phone Number',
+        checked: false,
+      },
+      {
+        value: 'User Location (1m)',
+        checked: false,
+      },
+      {
+        value: 'User Location (10m)',
+        checked: false,
+      },
+      {
+        value: 'User Location (100m)',
+        checked: false,
+      },
+      {
+        value: 'User Location (1km)',
+        checked: false,
+      },
+      {
+        value: 'User Location (10km)',
+        checked: false,
+      },
+    ],
+  },
+];
+
+export const attackerHardware = [
+  {
+    type: 'select',
+    options: [
+      {
+        value: 'Computer',
+        checked: false,
+      },
+      {
+        value: 'Software-Defined Radio (SDR)',
+        checked: false,
+      },
+      {
+        value: 'Commercial Radio (Wi-Fi)',
+        checked: false,
+      },
+      {
+        value: 'Commercial Radio (Bluetooth Classic)',
+        checked: false,
+      },
+      {
+        value: 'Commercial Radio (BLE)',
+        checked: false,
+      },
+      {
+        value: 'Commercial Radio (Zigbee)',
+        checked: false,
+      },
+      {
+        value: 'Commercial Radio (2G)',
+        checked: false,
+      },
+      {
+        value: 'Commercial Radio (3G)',
+        checked: false,
+      },
+      {
+        value: 'Commercial Radio (4G)',
+        checked: false,
+      },
+      {
+        value: 'Commercial Radio (5G-NSA)',
+        checked: false,
+      },
+      {
+        value: 'Commercial Radio (5G-SA)',
+        checked: false,
+      },
+      {
+        value: 'Traffic Sniffer (Wi-Fi)',
+        checked: false,
+      },
+      {
+        value: 'Traffic Sniffer (Bluetooth Classic)',
+        checked: false,
+      },
+      {
+        value: 'Traffic Sniffer (BLE)',
+        checked: false,
+      },
+      {
+        value: 'Traffic Sniffer (Zigbee)',
+        checked: false,
+      },
+      {
+        value: 'Traffic Sniffer (2G)',
+        checked: false,
+      },
+      {
+        value: 'Traffic Sniffer (3G)',
+        checked: false,
+      },
+      {
+        value: 'Traffic Sniffer (4G)',
+        checked: false,
+      },
+      {
+        value: 'Traffic Sniffer (5G)',
+        checked: false,
+      },
+      {
+        value: 'Fake Base Station (2G)',
+        checked: false,
+      },
+      {
+        value: 'Fake Base Station (3G)',
+        checked: false,
+      },
+      {
+        value: 'Fake Base Station (4G)',
+        checked: false,
+      },
+      {
+        value: 'Fake Base Station (5G-NSA)',
+        checked: false,
+      },
+      {
+        value: 'Fake Base Station (5G-SA)',
+        checked: false,
+      },
+    ],
+  },
+];
+
+export const attackerPostCondition = [...victimPostCondition];
